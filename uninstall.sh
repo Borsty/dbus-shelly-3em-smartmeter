27e30d763162adba8beb9f99953334b8db968df1
Kotty <kotty@guns-n-girls.de>
2024-06-01 15:51:47 +0200

    fixes

--- uninstall.sh
+++ uninstall.sh
@@ -3,9 +3,15 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  
 DAEMON_NAME=${SCRIPT_DIR##*/}
 rclocalname=/data/rc.local
 
-rm /service/$DAEMON_NAME
-kill $(pgrep -f "python $SCRIPT_DIR")
-kill $(pgrep -f 'supervise $DAEMON_NAME')
-chmod a-x $SCRIPT_DIR/service/run
+if [[ -L "/service/$DAEMON_NAME" && -d "/service/$DAEMON_NAME" ]]
+then
+  rm /service/$DAEMON_NAME
+fi
+
+python_pid=$(pgrep -f "python $SCRIPT_DIR")
+supervise_pid=$(pgrep -f 'supervise $DAEMON_NAME')
+
+[ ! -z $python_pid ] && kill $python_pid
+[ ! -z $supervise_pid ] && kill $supervise_pid
 
 grep -qxF "/bin/bash $SCRIPT_DIR/install.sh" $rclocalname && sed -i "#/bin/bash $SCRIPT_DIR/install.sh#d" $rclocalname
